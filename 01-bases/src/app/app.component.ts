efb808927f422193f7bd913aae3481d644871162
Daniel <djblancoh@gmail.com>
2022-04-24 15:56:24 -0500

    pruebas con propiedades entra html y ts

--- 01-bases/src/app/app.component.ts
+++ 01-bases/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template:'<span>Dany</span>',
+  // template:`
+  //   <h1>  Template</h1>
+  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public titulo: string = 'Contador';
+  public numero: number = 10;
+
+  public base: number = 5;
+
+  acumular( val: number ):number {
+    return this.numero += val;
+  }
+
 }
