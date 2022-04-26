7bdad598274173b2a25f19893bfa1a5b3caa1600
Daniel <djblancoh@gmail.com>
2022-04-25 19:38:14 -0500

    @Output, emision de evnetos

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,57 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
-
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
-
   public personajes: Personaje[] = [
     {
       nombre: 'Goku',
-      poder: 15000
+      poder: 15000,
     },
     {
       nombre: 'Vegeta',
-      poder: 7500
-    }
-  ]
+      poder: 7500,
+    },
+  ];
 
   public nuevo: Personaje = {
     nombre: '',
-    poder: 0
+    poder: 0,
+  };
+
+  agregarNuevoPersonaje( arg_personaje: Personaje) {
+    this.personajes.push(arg_personaje);
   }
 
-  agregar() {
-    if(this.nuevo.nombre.trim().length === 0){ return; }
-    console.log(this.nuevo);
-
-    this.personajes.push(this.nuevo);
-    console.log(this.personajes);
-
-    this.nuevo={
-      nombre: '',
-      poder: 0
-    }
-    // this.clearData(this.nuevo);
-  }
-
-  clearData(pj: Personaje): void{
-
-
-    pj = {
-      nombre: '',
-      poder: 0
-    }
-
-  }
-
-  cambiarNombre( event: any) {
+  cambiarNombre(event: any) {
     console.log(event.target.value);
-
   }
 }
