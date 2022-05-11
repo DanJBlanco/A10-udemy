58855ca986b55ad36510be31aaafc0ded062b49b
Daniel <djblancoh@gmail.com>
2022-05-11 10:47:36 -0500

    integrate routerlink with menu bar, and edit items

--- 04-pipesApp/src/app/shared/menu/menu.component.ts
+++ 04-pipesApp/src/app/shared/menu/menu.component.ts
@@ -11,28 +11,33 @@ export class MenuComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {  
+  ngOnInit(): void {
     this.items = [
       {
-        label: 'File',
+        label: 'Angular Pipes',
+        icon: 'pi pi-desktop',
         items: [
           {
-            label: 'New',
-            icon: 'pi pi-fw pi-plus',
-            items: [{ label: 'Project' }, { label: 'Other' }],
+            label: 'Text & Date',
+            icon: 'pi pi-align-left',
+            routerLink: '/'
           },
-          { label: 'Open' },
-          { label: 'Quit' },
-        ],
+          {
+            label: 'Numbers',
+            icon: 'pi pi-dollar',
+            routerLink: 'numbers'
+          },
+          {
+            label: 'No Commons',
+            icon: 'pi pi-globe',
+            routerLink: 'no-commons'
+          }
+        ]
       },
       {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' },
-        ],
-      },
+        label: 'Custom Pipes',
+        icon: 'pi pi-cog'
+      }
     ];
   }
 }
