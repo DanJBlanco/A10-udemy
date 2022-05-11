8275f91093afc859616e1ab288b999cd7405921b
Daniel <djblancoh@gmail.com>
2022-05-11 12:43:45 -0500

    date pipe, and custom format

--- 04-pipesApp/src/app/sells/pages/basics/basics.component.ts
+++ 04-pipesApp/src/app/sells/pages/basics/basics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basics',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicsComponent implements OnInit {
+export class BasicsComponent{
 
-  constructor() { }
+  nameUpper: string = 'DANIEL';
+  nameLower: string = 'daniel';
+  fullName: string = 'dAniEl';
 
-  ngOnInit(): void {
-  }
+  date: Date = new Date();
 
 }
