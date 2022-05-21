15a7fad46fd04a38a172c12c910f14e6a3a45db4
Daniel <djblancoh@gmail.com>
2022-05-21 17:52:06 -0500

    create reactive inputs, add delete function

--- 06-FormsApp/src/app/template/dinamics/dinamics.component.ts
+++ 06-FormsApp/src/app/template/dinamics/dinamics.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+interface Customer {
+  name: string;
+  favorities?: Favorite[]
+}
+interface Favorite {
+  id: string;
+  name: string;
+}
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
   styles: [
   ]
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
 
-  constructor() { }
+  @ViewChild('myDinamicForm') myForm!: NgForm;
 
-  ngOnInit(): void {
+  customer: Customer = {
+    name: 'Name1',
+    favorities: [
+      { id:"1", name:'fav1'},
+      { id:"2", name:'fav2'}
+    ]
   }
 
+  save() {
+    console.log('Post form');
+  }
+
+  delete(index: number) {
+    this.customer.favorities?.splice(index,1);
+  }
 }
