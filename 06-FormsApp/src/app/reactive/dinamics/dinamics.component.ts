675afd7d3c929969b64d5ec4800c3c981f4b42eb
Daniel <djblancoh@gmail.com>
2022-05-22 01:20:48 -0500

    add dianmics inputs

--- 06-FormsApp/src/app/reactive/dinamics/dinamics.component.ts
+++ 06-FormsApp/src/app/reactive/dinamics/dinamics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamics',
@@ -8,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicsComponent implements OnInit {
 
-  constructor() { }
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favorities: this.fb.array([
+      ['Game1'],
+      ['GAme2']
+    ], Validators.required),
+
+  })
+
+  get favArr(){
+    return this.myForm.get('favorities') as FormArray;
+  }
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  isValidField(field: string){
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  save(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset();
   }
 
 }
