77e7ccd024c145d4c020e6fcf2b24b3c040de699
Daniel <djblancoh@gmail.com>
2022-05-21 23:40:10 -0500

    add switchs forms

--- 06-FormsApp/src/app/template/switchs/switchs.component.ts
+++ 06-FormsApp/src/app/template/switchs/switchs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switchs',
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SwitchsComponent{
+  user = {
+    gender: '',
+    notification: false
   }
 
+  termAndConditions: boolean = false;
 }
