291a46f0fe233986d40af6ff0f8aef1e906ddd71
Daniel <djblancoh@gmail.com>
2022-08-02 23:15:49 -0500

    implement mapbox lib

--- 09-mapApps/src/app/maps/pages/full-screen/full-screen.component.ts
+++ 09-mapApps/src/app/maps/pages/full-screen/full-screen.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../../../environments/environment';
+
 
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
-  styles: [
+  styles: [`
+    #map {
+      width: 100%;
+      height: 100%;
+    }
+  `
   ]
 })
 export class FullScreenComponent implements OnInit {
 
+ 
   constructor() { }
 
   ngOnInit(): void {
+
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+      });
+      map.on('style.load', () => {
+      map.setFog({}); // Set the default atmosphere style
+      });
   }
 
 }
--- 09-mapApps/src/environments/environment.ts
+++ 09-mapApps/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  mapboxToken: 'pk.eyJ1IjoiZGFud2h5IiwiYSI6ImNsNmE2Ynl3OTRjaHIzZG8xdDBrZmk0cXkifQ.pNDwW1pPLD_9JqxiE0qkng',
 };
 
 /*
